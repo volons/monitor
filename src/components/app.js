c6fa101a979d9b596002a2fd8c285236c20b4d3d
Vincent Karoubi <vincent.karoubi@gmail.com>
2019-07-05 22:25:20 +0200

    Add prettier

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import { h, Component } from 'preact';
-import Helmet from "preact-helmet";
+import Helmet from 'preact-helmet';
 import { Router } from 'preact-router';
 
 import Header from './header';
@@ -9,32 +9,27 @@ import Home from '../routes/home';
 import About from '../routes/about';
 
 export default class App extends Component {
+    handleRoute = e => {
+        this.currentUrl = e.url;
+    };
 
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+    render() {
+        return (
+            <div id="app">
+                <Helmet
+                    htmlAttributes={{ lang: 'en', amp: undefined }} // amp takes no value
+                    title="Volons - Monitor"
+                    titleAttributes={{ itemprop: 'name', lang: 'en' }}
+                    meta={[{ name: 'charset', content: 'UTF-8' }]}
+                />
 
-	render() {
-		return (
-			<div id="app">
+                <Header />
 
-				<Helmet
-					htmlAttributes={{lang: "en", amp: undefined}} // amp takes no value
-					title="Volons - Monitor"
-					titleAttributes={{itemprop: "name", lang: "en"}}
-					meta={[
-						{name: "charset", content: "UTF-8"},
-					]}
-				/>
-
-				<Header />
-
-				<Router onChange={this.handleRoute}>
-					<Home path="/" />
-					<About path="/about" />
-				</Router>
-
-			</div>
-			);
-	}
+                <Router onChange={this.handleRoute}>
+                    <Home path="/" />
+                    <About path="/about" />
+                </Router>
+            </div>
+        );
+    }
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const Config = {
-	HiveConnectionString: 'ws://localhost:8656/admin',
-	GoogleMapApiKey: 'AIzaSyBgI0hsdvPO4K4DGyjBAWRymP2q7QoSzDQ'
+    HiveConnectionString: 'ws://localhost:8656/admin',
+    GoogleMapApiKey: 'AIzaSyBgI0hsdvPO4K4DGyjBAWRymP2q7QoSzDQ',
 };
 
 export default Config;
